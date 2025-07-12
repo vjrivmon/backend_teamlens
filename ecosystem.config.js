360b01894654e0d77da02ceaf1059bdf6d75f7a1
Vicente Rivas Monferrer <vjrivmon@epsg.upv.es>
2025-07-12 07:54:22 +0200

    Cambios Vicente | Mejora en el servicio de correos

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   apps: [{
     name: 'teamlens-backend',
-    script: 'build/index.js',
-    instances: 1,
-    exec_mode: 'fork',
+    script: './build/index.js',
     watch: false,
-    max_memory_restart: '1G',
+    env_file: '.env',
+    env: {
+      NODE_ENV: 'production',
+      FRONTEND_URL: 'http://teamlens.gti-ia.dsic.upv.es'
+    },
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
     error_file: './logs/err.log',
     out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true,
-    restart_delay: 3000
+    log_file: './logs/combined.log'
   }]
 }
